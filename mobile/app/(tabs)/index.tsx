0c1ef4d31228d2e3acf33606488a98eed8bed090
Kathrin <kathrin.pleuger@icloud.com>
2025-06-13 20:50:41 +0200

    refactor: load user data via userService and handle loading & errors

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -2,13 +2,37 @@ import List from '@/components/habit/HabitList';
 import GamificationCard from '@/components/Gamification/GamificationCard';
 import Container from '@/components/Container';
 import TopBar from '@/components/TopBar';
+import {useEffect, useState} from "react";
+import {UserData} from "@/types/user";
+import {fetchUserData} from "@/services/userService";
+import {Text} from "react-native";
 
 export default function HomeScreen() {
-  return (
-    <Container>
-      <TopBar tab="home"/>
-      <GamificationCard hp={29} xp={12} maxXp={50} lvl={1} />
-      <List />
-    </Container>
-  );
+    const [userData, setUserData] = useState<UserData | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<Error | null>(null);
+
+    useEffect(() => {
+        fetchUserData()
+            .then(data => {
+                setUserData(data);
+            })
+            .catch(err => {
+                console.error("Fehler beim Fetch:", err);
+                setError(err);
+            })
+            .finally(() => setLoading(false));
+    }, []);
+
+    if (loading) return <Text>Loading...</Text>;
+    if (error) return <Text>Fehler beim Laden der Daten</Text>;
+    if (!userData) return <Text>Keine Daten</Text>;
+
+    return (
+        <Container>
+            <TopBar tab="home"/>
+            <GamificationCard userData={userData}/>
+            <List/>
+        </Container>
+    );
 }
